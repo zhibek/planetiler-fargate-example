e0fc95b1407a52600f27cd2ba17405a6b807f204
John Levermore <john.levermore@zhibek.com>
2023-11-27 16:25:52 +0000

    Configure Planetiler to run

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
 FROM ghcr.io/onthegomap/planetiler:latest
 
+# Install AWS cli
+RUN apt update \
+&& apt install -y unzip \
+&& cd /tmp \
+&& curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+&& unzip awscliv2.zip \
+&& ./aws/install \
+&& aws --version \
+&& cd -
+
 # Setup workdir
 WORKDIR /workspace
 
--- run.sh
+++ run.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 # Stop on any error
-set -x
+set -e
 
-# Show we've run
-echo "Hello world!"
+# Use env vars or defaults for input variables
+AREA="${AREA:=}"
+BUCKET="${BUCKET:=}"
+
+# Validate input variables are set
+[ -z "${AREA}" ] && echo "ERROR: AREA env var must be set!" && exit 1;
+[ -z "${BUCKET}" ] && echo "ERROR: BUCKET env var must be set!" && exit 1;
+
+# Execute Planetiler
+echo "** Running Planetiler..."
+java -Xmx6g \
+  -cp @/app/jib-classpath-file com.onthegomap.planetiler.Main \
+  --download \
+  --area=${AREA} \
+  --output=${AREA}.pmtiles
+echo "** Planetiler complete!"
+
+echo "** Checking data files..."
+ls -alh ./${AREA}.pmtiles
+
+echo "** Save PMTiles to S3 bucket"
+aws s3 cp \
+  ./${AREA}.pmtiles \
+  s3://${BUCKET}
